a06e26015b3da4d54290d36b5a896bd8265e1ec6
Leandro Cruz <LeandroCruz717@outlook.com>
2023-08-30 17:03:22 -0300

    feat(environment): add API_ENDPOINT entry

--- src/environment.ts
+++ src/environment.ts
@@ -16,6 +16,7 @@ const environment = Object.freeze({
         host: '0.0.0.0',
         port: 3000,
         secure: process.env.HTTPS === 'true',
+        url: process.env.API_ENDPOINT,
         entities: [
             __dirname + '/**/*.entity.js',
             __dirname + '/**/*.entity.ts',
