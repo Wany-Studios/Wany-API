467b605c9a8f7387f1ade49d0a7be82464f45232
Leandro Cruz <LeandroCruz717@outlook.com>
2023-08-30 17:22:00 -0300

    feat(get-routes): correct routes return

--- src/helpers/get-routes.helper.ts
+++ src/helpers/get-routes.helper.ts
@@ -1,14 +1,18 @@
+import environment from '../environment';
+
 export function getRoutes() {
+    const _ = (path: string) => new URL(environment.server.url, path).href;
+
     return {
-        avatar_url: 'public/{username}/avatar',
-        current_user_url: 'user/me',
-        forgot_password_url: 'auth/forgot-password',
-        login_url: 'auth/signin',
-        logout_url: 'auth/logout',
-        reset_password_url: 'auth/reset-password',
-        signup_url: 'auth/signup',
-        user_url: 'user/public/{username}',
-        upload_avatar_url: 'user/upload-avatar',
-        verify_email_url: 'auth/email/verification?token={token}',
+        avatar_url: _('public/{username}/avatar'),
+        current_user_url: _('user/me'),
+        forgot_password_url: _('auth/forgot-password'),
+        login_url: _('auth/signin'),
+        logout_url: _('auth/logout'),
+        reset_password_url: _('auth/reset-password'),
+        signup_url: _('auth/signup'),
+        user_url: _('user/public/{username}'),
+        upload_avatar_url: _('user/upload-avatar'),
+        verify_email_url: _('auth/email/verification?token={token}'),
     };
 }
