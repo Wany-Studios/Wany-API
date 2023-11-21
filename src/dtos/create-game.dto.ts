81e1be68206e26fb984182b438bcb58a3784dd48
Leandro Cruz <LeandroCruz717@outlook.com>
2023-11-20 21:10:20 -0300

    feat(game): update create-game dto fields validation

--- src/dtos/create-game.dto.ts
+++ src/dtos/create-game.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Genre } from '../modules/models/genre';
-import { IsEnum, IsString } from 'class-validator';
+import { IsEnum, IsString, Matches, MinLength } from 'class-validator';
 import { RemoveExtraSpaces } from '../helpers/class-validator/remove-extra-spaces.decorator';
 import { Transform } from 'class-transformer';
 
@@ -18,7 +18,11 @@ export abstract class CreateGameDto {
         minLength: 2,
     })
     @IsString()
+    @Matches(/^[A-Za-z0-9_\-\s\p{Emoji}]+$/, {
+        message: 'Invalid characters in the title',
+    })
     @RemoveExtraSpaces()
+    @MinLength(3)
     title: string;
 
     @ApiProperty({
@@ -27,6 +31,7 @@ export abstract class CreateGameDto {
     })
     @IsString()
     @RemoveExtraSpaces()
+    @MinLength(1)
     @Transform(({ value }) => value && value.toLowerCase())
     description: string;
 
